ca8389eea7ad2575c387e0b46ff2fabbac1d17c5
caybar <aybarcelal@gmail.com>
2018-02-17 18:31:52 +0300

    Create soy-agaci-konsol-kodu.js

---
+++ soy-agaci-konsol-kodu.js
@@ -0,0 +1,4 @@
+var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://cdn.rawgit.com/caybar/e-devlet-soy-agaci/master/soy-agaci-olustur3.js";
+    document.head.appendChild(script);
