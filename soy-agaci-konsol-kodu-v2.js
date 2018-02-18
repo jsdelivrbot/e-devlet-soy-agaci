f6a7f1878788737c7a865c88d8aea7f83d208488
caybar <aybarcelal@gmail.com>
2018-02-18 22:24:57 +0300

    Delete soy-agaci-konsol-kodu-v2.js

--- soy-agaci-konsol-kodu-v2.js
+++
@@ -1,4 +0,0 @@
-var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "https://cdn.rawgit.com/caybar/e-devlet-soy-agaci/master/soy-agaci-olustur3.js";
-    document.head.appendChild(script);
