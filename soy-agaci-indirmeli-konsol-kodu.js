afae52d8b6cd1cf93cabf4f938bcc8aab0c8abaa
caybar <aybarcelal@gmail.com>
2018-02-21 19:35:40 +0300

    Create soy-agaci-indirmeli-konsol-kodu.js
    Soy ağacını olusturur ve resim olarak indirir.

---
+++ soy-agaci-indirmeli-konsol-kodu.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdn.rawgit.com/caybar/e-devlet-soy-agaci/master/soy-agaci-olustur-indir.js';
+document.head.appendChild(script);
